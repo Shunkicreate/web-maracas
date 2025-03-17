721f46df38b53ae2ee43fcd536f3e61ad2d5d926
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2025-03-18 00:50:12 +0900

    ga4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const zenkakugothicnew = Zen_Kaku_Gothic_New({
 	weight: "400",
@@ -50,6 +51,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 				className={`${zenkakugothicnew.className} bg-background dark:bg-darkbackground text-font dark:text-darkfont`}
 			>
 				<ChakraProvider>
+					<GoogleAnalytics gaId="G-HTPY1W81KH" />
 					<Header />
 					<main className='min-h-[calc(100svh-1.5rem)]'>
 						<div className='md:max-w-[60%] max-w-[90%] m-auto pt-20'>{children}</div>
